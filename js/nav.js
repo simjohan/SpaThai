fab73823b574855307a3eefffe9f7efa744cbdd8
Cornelius Grieg Dahling <corneliusgriegdahling@dhcp-10-20-60-252.wlan.ntnu.no>
2015-11-12 18:20:06 +0100

    update nav.js to support navigation_scroll

--- js/nav.js
+++ js/nav.js
@@ -7,6 +7,7 @@ window.onload = function(){
   currentURL = "\""+currentURL+"\"";
 
   var navbar = "";
+  var navbar_scroll = "";
   var links = [["HJEM",'"index.html"'],
       ["BEHANDLINGER",'"treatments.html"'],
       ["PRISER",'"prices.html"'],
@@ -24,14 +25,18 @@ window.onload = function(){
     //if it is the currentURL make it active
     if(currentURL == links[i][1]){
       navbar += "<a class=\"active\" href="+links[i][1]+">"+links[i][0]+"</a>";
+      navbar_scroll += "<a class=\"active_nav_scroll\" href="+links[i][1]+">"+links[i][0]+"</a>";
     }else{
       navbar += "<a href="+links[i][1]+">"+links[i][0]+"</a>";
+      navbar_scroll += "<a href="+links[i][1]+">"+links[i][0]+"</a>";
     }
   }
   document.getElementById("navigation").innerHTML = navbar;
+  document.getElementsByClassName("navigation_scroll_nav")[0].innerHTML = navbar_scroll;
   //navbar += "</nav>";
 
   console.log(navbar);
+  console.log(navbar_scroll);
 
 
 
