9a3a96c93a471abffa595b1c43dfb9bd7695411d
Cornelius Grieg Dahling <corneliusgriegdahling@dhcp-10-20-60-252.wlan.ntnu.no>
2015-11-12 18:29:29 +0100

    Added comments to navigation_scroll.js

--- js/navigation_scroll.js
+++ js/navigation_scroll.js
@@ -1,10 +1,14 @@
-$(document).ready(function(){       
+$(document).ready(function(){ 
+
+// checking if user started to scroll      
    $(document).scroll(function() { 
+
+    // if scrolled >= 140px from top, hide main nav and fade in hidden nav.
       if($(document).scrollTop() >= 140) {
           $('.nav').hide();
           $('.navigation_scroll').fadeIn('slow');
       } 
-
+    // if scrolled back to <= 60px, show main nav and fadeout hidden nav.
        else if($(document).scrollTop() <= 60){
           $('.nav').show();
           $('.navigation_scroll').fadeOut('fast');
